1ba5318eba434de7da10be188975a5c8b13be314
mrlthf11 <mrlth@qq.com>
2020-11-22 14:52:24 +0800

    upd week 04

---
+++ Week 04/kmp.js
@@ -0,0 +1,40 @@
+const kmp = (source, pattern) => {
+    const table = Array(pattern.length).fill(0)
+
+    {
+        let i = 1, j = 0
+        while (i < pattern.length) {
+            if (pattern[i] === pattern[j]) {
+                ++i, ++j
+                table[i] = j
+            } else {
+                if (j > 0) {
+                    j = table[j]
+                } else {
+                    ++i
+                }
+            }
+        }
+    }
+    {
+        let i = 0, j = 0
+        while (i < source.length) {
+            if (pattern[j] === source[i]) {
+                ++i, ++j
+            } else {
+                if (j > 0) {
+                    j = table[j]
+                } else {
+                    ++i
+                }
+            }
+            if (j === pattern.length)
+                return true
+        }
+        return false
+    }
+
+    return table
+}
+
+console.log(kmp('', 'aaabaaaac'))
