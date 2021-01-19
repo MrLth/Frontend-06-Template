93bbc462b5e3bffb483dbea4533a6b29d00ce555
mrlth <mrlth@qq.com>
2021-01-19 15:14:07 +0800

    upd week 13

--- Week 13/proxy.js
+++ Week 13/proxy.js
@@ -2,20 +2,33 @@
  * @Author: mrlthf11
  * @LastEditors: mrlthf11
  * @Date: 2021-01-19 13:25:20
- * @LastEditTime: 2021-01-19 13:52:22
+ * @LastEditTime: 2021-01-19 15:13:14
  * @Description: file content
  */
 
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-// const path = require('path')
+const fs = require('fs');
+const path = require('path')
+const https = require('https');
+const http = require('http');
 
 const app = express();
 
-app.use('/api', createProxyMiddleware({ target: 'https://baidu.com', changeOrigin: true }));
+const privateKey = fs.readFileSync(path.resolve(__dirname, './private.pem'), 'utf8');
+const certificate = fs.readFileSync(path.resolve(__dirname, './file.crt'), 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
-app.use('/static', express.static(__dirname))
 
-// console.log(typeof express.static)
 
-app.listen(3000);
+
+app.use('/api', createProxyMiddleware({
+    target: 'https://baidu.com',
+    changeOrigin: true,
+    pathRewrite: { '/api': '/' }
+}));
+
+app.use('/', express.static(__dirname))
+
+http.createServer(app).listen(3000)
+https.createServer(credentials, app).listen(3001)
