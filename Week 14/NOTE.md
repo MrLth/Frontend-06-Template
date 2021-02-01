384dc90a6596e0d0f16c767a8228853886eed4b5
mrlthf11 <mrlth@qq.com>
2021-02-01 21:32:31 +0800

    upd week 14

--- Week 14/NOTE.md
+++ Week 14/NOTE.md
@@ -1,14 +1,17 @@
-目前已知的情报
+# 目前已知的情报
 
 1. requestAnimationFrame 添加的回调的执行时机
-   1. 当前帧至下一帧之间，且每次都会清空在下一帧之前添加的所有 requestAnimationFrame 任务，而被递归调用的 requestAnimationFrame 生成的任务会在下下帧被执行
+   1. 当前帧至下一帧之间，且每次都会清空在下一帧之前添加的所有 requestAnimationFrame 任务，而被递归调用的 requestAnimationFrame 所生成的任务会在下下帧被执行
    2. 所有 requestAnimationFrame 任务都会在 Recalulate Style 和 Upadate Layer Tree 以及 Composite Layers 之前被执行
+2. mousedown，mousemove，mouseup  事件的事件处理回调在执行前都会执行一次 Upadate Layer Tree 任务，与回调内是否有读写样式无关。
+3. 如果在事件回调函数中进行写样式，就会在回调执行完毕后，在同一宏任务内完成一次 Recalulate Style 和 Upadate Layer Tree 以及 Composite Layers 三连。而定时器任务的写样式的三连不在同一宏任务内
+4. 在 chrome中，mousemove 16.8ms 执行一次，这和 requestAnimationFrame 很相似
 
-技巧
+# 技巧
 
-1. 使数组下标支持倒数第 n 个元素，n = (input + arr.length) % arr.length
+1. 使数组下标支持倒数第 n 个元素，n = (input + arr.length) % arr.length，但当 input < - arr.length 时就会出现下标越界
 
-最新情报
+# 最新情报
 
 1. 位运算的优先级比普通运算符更低，而不是和乘除类似
    https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
@@ -31,10 +34,10 @@
    Math.round(x) // -0
    Math.round(-0.50) // -0
    Math.round(-0.51) // -1
-   
-   // 辅助记忆
-   位运算大概精度 32 位，每 10 位二进制位表示 1024，即 1024*1024*1024，简单记为 1000**3，即 32 位二进制位描述十进制的精度大概在 1000**(32/10) == 1E9 左右
-   
    ```
-
    
+
+# 辅助记忆
+位运算大概精度 32 位，每 10 位二进制位表示 1024，即 1024\*1024\*1024，简单记为 1000\**3，即10亿这个量级，`2**31 = 2147483648`
+
+[![ye0z7R.png](https://s3.ax1x.com/2021/02/01/ye0z7R.png)](https://imgchr.com/i/ye0z7R)
--- Week 14/mouse.js
+++ Week 14/mouse.js
@@ -2,7 +2,7 @@
  * @Author: mrlthf11
  * @LastEditors: mrlthf11
  * @Date: 2021-01-30 16:18:13
- * @LastEditTime: 2021-01-31 19:03:16
+ * @LastEditTime: 2021-02-01 21:18:00
  * @Description: file content
  */
 import { createElement, Component } from './happyReact'
@@ -24,18 +24,17 @@ class Carousel extends Component {
 
     this.root.addEventListener('mousedown', (e) => {
       const startX = e.clientX
-      width = width ?? this.root.getBoundingClientRect().width ?? 500
-      console.log(width);
-
+      // TODO: 观察 performance 是否会减少一次重绘
+      // width = width ?? this.root.getBoundingClientRect().width ?? 500
+      // width = this.root.getBoundingClientRect().width ?? 500
+      width = 500
 
       const move = (e) => {
         const x = e.clientX - startX
-        const current = position - (x / width | 0)
-        // const current = position - ((x - x % 888) / 888)
-        for (const offset of [-2, -1, 0, 1, 2]) {
+        const current = position - (x / width | 0) // position - ((x - x % 888) / 888)
+        for (const offset of [-1, 0, 1]) {
           let pos = current + offset;
-          pos = pos < 0 ? len - 1 + pos % len : pos % len
-          // pos = (pos + children.length) % children.length
+          pos = pos < 0 ? len - 1 + pos % len : pos % len // (pos + children.length) % children.length
           children[pos].style.transition = 'none'
           children[pos].style.transform = `translateX(${-pos * width + offset * width + x % width}px)`
         }
@@ -45,9 +44,7 @@ class Carousel extends Component {
         position = position - Math.round(x / width)
         for (const offset of [0, -Math.sign(Math.round(x / width) - x + width / 2 * Math.sign(x))]) {
           let pos = position + offset;
-          console.log(pos);
           pos = pos < 0 ? len - 1 + pos % len : pos % len
-          console.log(pos);
           children[pos].style.transition = ''
           children[pos].style.transform = `translateX(${-pos * width + offset * width}px)`
         }
@@ -63,9 +60,8 @@ class Carousel extends Component {
 }
 
 (<Carousel src={[
-  './images/1.jpg',
-  './images/2.jpg',
-  './images/3.jpg',
-  './images/4.jpg',
-  './images/5.jpg',
+  "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+  "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
+  "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+  "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg"
 ]} />).mount(document.body)
