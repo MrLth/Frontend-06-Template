ecc457d3000a3d3d78ca69031107260f89012889
mrlthf11 <mrlth@qq.com>
2021-03-05 00:57:43 +0800

    update week 19

--- Week 19/publish-server/index.js
+++ Week 19/publish-server/index.js
@@ -2,28 +2,64 @@
  * @Author: mrlthf11
  * @LastEditors: mrlthf11
  * @Date: 2021-03-04 16:42:12
- * @LastEditTime: 2021-03-04 17:04:20
+ * @LastEditTime: 2021-03-05 00:51:36
  * @Description: file content
  */
 const http = require('http')
-const fs = require('fs')
+const https = require('https')
+const unzipper = require('unzipper')
 const { resolve: _resolve } = require('path')
 
 const resolve = _resolve.bind(null, __dirname)
 
-console.log('123')
-http.createServer((req, res) => {
+const clientId = 'Iv1.3523ac288f0f5c54'
+const clientSecret = 'a4db7794801f5cc693f824a7edb3b3926c29ad2a'
 
+function oAuth(req, res) {
+  const code = new URLSearchParams(req.url.split('?')[1]).get('code')
+  getToken(code)
+    .then((token) => {
+      res.end(`<a href='http://localhost:3002/publish?token=${token}'>publish</a>`)
+    })
+    .catch(err => {
+      res.end(err)
+    })
+}
 
-  console.log('111')
-  // const file = fs.createWriteStream(resolve('./public/index.html'))
-
-  // req.pipe(file)
-  req.on('data', chunk => console.log(chunk.toString()))
-
-  req.on('end', () => {
-    console.log('hello world')
-    res.end('hello world')
+function getToken(code) {
+  return new Promise((resolve, reject) => {
+    https.request({
+      hostname: 'github.com',
+      path: `/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`,
+      method: 'POST',
+    }, res => {
+      let rstStr = ''
+      res.on('data', chunk => rstStr += chunk.toString())
+      res.on('end', () => {
+        const params = new URLSearchParams(rstStr)
+        console.log(rstStr)
+        if (params.has('access_token')) {
+          resolve(params.get('access_token'))
+        } else {
+          reject(rstStr)
+        }
+      })
+    }).end()
   })
+}
 
+
+function publish(req, res) {
+  req.pipe(unzipper.Extract({ path: resolve('public') }))
+  req.on('end', () => {
+    console.log('download successfully')
+    res.end('upload successfully')
+  })
+}
+
+http.createServer((req, res) => {
+  console.log(req.url)
+  if (req.url.startsWith('/oAuth?code=')) {
+    oAuth(req, res)
+  }
 }).listen(3001)
--- Week 19/publish-tool/index.js
+++ Week 19/publish-tool/index.js
@@ -2,45 +2,42 @@
  * @Author: mrlthf11
  * @LastEditors: mrlthf11
  * @Date: 2021-03-04 16:47:59
- * @LastEditTime: 2021-03-04 17:07:54
+ * @LastEditTime: 2021-03-05 00:43:56
  * @Description: file content
  */
 const http = require('http')
-const fs = require('fs')
-
+const archiver = require('archiver');
 const { resolve: _resolve } = require('path')
+const childProcess = require('child_process')
+
 const resolve = _resolve.bind(null, __dirname)
 
+childProcess.exec('open https://github.com/login/oauth/authorize?client_id=Iv1.3523ac288f0f5c54')
 
-const filePath = resolve('./public/index.html')
+const archive = archiver('zip', {
+  zlib: { level: 9 } // Sets the compression level.
+})
 
-
-fs.stat(filePath, (err, stat) => {
-  // console.log(stat)
-  const file = fs.createReadStream(filePath)
-
+function publish() {
   const request = http.request({
     hostname: 'localhost',
     port: 3001,
     method: 'POST',
     headers: {
       'Content-type': 'application/octet-stream',
-      'Content-length': stat.size
     }
   }, res => {
-    console.log('success')
+    let rstStr = ''
+    res.on('data', chunk => rstStr += chunk.toString())
+    res.on('end', () => console.log(rstStr))
   })
 
+  archive.directory(resolve('public'), false);
+  archive.pipe(request)
+  archive.finalize();
+  archive.on('end', () => request.end())
+}
 
-  file.on('data', chunk => {
-    request.write(chunk)
-    console.log(chunk.toString())
-  })
-  // file.pipe(request)
-  file.on('end', () => {
-    console.log('end')
-    request.end()
-  })
-})
-
-
+http.createServer((req, res) => {
+  console.log(req)
+}).listen(3002)
