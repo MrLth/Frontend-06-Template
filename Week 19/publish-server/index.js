602a4b08e6e286799cfa8745c80a8a2d4f819ce4
mrlthf11 <mrlth@qq.com>
2021-03-05 11:09:26 +0800

    upd week19

--- Week 19/publish-server/index.js
+++ Week 19/publish-server/index.js
@@ -2,24 +2,25 @@
  * @Author: mrlthf11
  * @LastEditors: mrlthf11
  * @Date: 2021-03-04 16:42:12
- * @LastEditTime: 2021-03-05 08:18:46
+ * @LastEditTime: 2021-03-05 11:08:44
  * @Description: file content
  */
 const http = require('http')
 const https = require('https')
 const unzipper = require('unzipper')
-const { resolve: _resolve } = require('path')
+const path = require('path')
 
-const resolve = _resolve.bind(null, __dirname)
+const p = path.resolve.bind(null, __dirname)
 
 const clientId = 'Iv1.3523ac288f0f5c54'
 const clientSecret = 'a4db7794801f5cc693f824a7edb3b3926c29ad2a'
 
 function oAuth(req, res) {
   const code = new URLSearchParams(req.url.split('?')[1]).get('code')
+  console.log('oAuth code', code)
   getToken(code)
     .then((token) => {
-      res.end(`<a href='http://localhost:3002/authorization?token=${token}'>publish</a>`)
+      res.end(`<a href='http://localhost:3002/publish?token=${token}'>publish</a>`)
     })
     .catch(err => {
       res.end(err)
@@ -37,7 +38,6 @@ function getToken(code) {
       res.on('data', chunk => rstStr += chunk.toString())
       res.on('end', () => {
         const params = new URLSearchParams(rstStr)
-        console.log(rstStr)
         if (params.has('access_token')) {
           resolve(params.get('access_token'))
         } else {
@@ -48,18 +48,52 @@ function getToken(code) {
   })
 }
 
-
 function publish(req, res) {
-  req.pipe(unzipper.Extract({ path: resolve('public') }))
-  req.on('end', () => {
-    console.log('download successfully')
-    res.end('upload successfully')
+  const token = req.headers.token
+  getUser(token)
+    .then((info) => {
+      if (info.login === 'MrLth') {
+        req.pipe(unzipper.Extract({ path: p('public') }))
+        req.on('end', () => {
+          console.log('download successfully')
+          res.end('upload successfully')
+        })
+      } else {
+        res.end('Permission denied')
+      }
+    })
+}
+
+function getUser(token) {
+  return new Promise((resolve, reject) => {
+    https.request({
+      hostname: 'api.github.com',
+      method: 'GET',
+      path: '/user',
+      headers: {
+        'Authorization': `token ${token}`,
+        'User-Agent': 'extension-publish'
+      }
+    }, res => {
+      let body = ''
+      res.on('data', chunk => body += chunk.toString())
+      res.on('end', () => {
+        try {
+          resolve(JSON.parse(body))
+        } catch (err) {
+          reject(body)
+        }
+      })
+    }).end()
   })
 }
 
+
 http.createServer((req, res) => {
-  console.log(req.url)
+  console.log('path', req.url)
   if (req.url.startsWith('/oAuth?code=')) {
     oAuth(req, res)
+  } else if (req.url.startsWith('/publish')) {
+    publish(req, res)
   }
 }).listen(3001)
--- Week 19/publish-tool/index.js
+++ Week 19/publish-tool/index.js
@@ -2,7 +2,7 @@
  * @Author: mrlthf11
  * @LastEditors: mrlthf11
  * @Date: 2021-03-04 16:47:59
- * @LastEditTime: 2021-03-05 08:44:43
+ * @LastEditTime: 2021-03-05 11:04:01
  * @Description: file content
  */
 const http = require('http')
@@ -11,64 +11,48 @@ const archiver = require('archiver');
 const { resolve: _resolve } = require('path')
 const childProcess = require('child_process')
 
-const resolve = _resolve.bind(null, __dirname)
+const p = _resolve.bind(null, __dirname)
 
-childProcess.exec('open https://github.com/login/oauth/authorize?client_id=Iv1.3523ac288f0f5c54')
 
-const archive = archiver('zip', {
-  zlib: { level: 9 } // Sets the compression level.
-})
 
-function publish() {
-  const request = http.request({
-    hostname: 'localhost',
-    port: 3001,
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/octet-stream',
-    }
-  }, res => {
-    let rstStr = ''
-    res.on('data', chunk => rstStr += chunk.toString())
-    res.on('end', () => console.log(rstStr))
-  })
-
-  archive.directory(resolve('public'), false);
-  archive.pipe(request)
-  archive.finalize();
-  archive.on('end', () => request.end())
-}
-
-function authorization(req, res) {
+function publish(req, res) {
   const token = new URLSearchParams(req.url.split('?')[1]).get('token')
-  authorizationRequest(token)
-    .then((rst) => {
-      console.log(rst)
+  publishRequest(token)
+    .then((res1) => {
+      console.log(res1)
+      res.end(res1)
     })
 }
 
-function authorizationRequest(token) {
+function publishRequest(token) {
   return new Promise((resolve, reject) => {
-    https.request({
-      hostname: 'api.github.com',
-      method: 'GET',
-      path: '/user',
+    const request = http.request({
+      hostname: 'localhost',
+      port: 3001,
+      method: 'POST',
+      path: `/publish?token=${token}`,
       headers: {
-        'Authorization': `token ${token}`,
-        'User-Agent': 'extension-publish'
-      }
+        'Content-Type': 'application/octet-stream',
+        'Token': token
+      },
     }, res => {
-      let rstStr = ''
-      res.on('data', chunk => rstStr += chunk.toString())
-      res.on('end', () => {
-        resolve(rstStr)
-      })
+      let body = ''
+      res.on('data', chunk => body += chunk.toString())
+      res.on('end', () => resolve(body))
     })
+
+    const archive = archiver('zip', { zlib: { level: 9 } })
+    archive.directory(p('public'), false);
+    archive.finalize();
+    archive.pipe(request)
   })
 }
+
 
 http.createServer((req, res) => {
-  if (req.url.startsWith('/authorization?token=')) {
-    authorization(req, res)
+  if (req.url.startsWith('/publish?token=')) {
+    publish(req, res)
   }
 }).listen(3002)
+
+childProcess.exec('open https://github.com/login/oauth/authorize?client_id=Iv1.3523ac288f0f5c54')
