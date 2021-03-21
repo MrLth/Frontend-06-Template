d6d3d068162f424c890f93e16645cab118dff0ea
mrlthf11 <mrlth@qq.com>
2021-03-21 17:34:38 +0800

    update week20

---
+++ Week 20/git-test/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: mrlthf11
+ * @LastEditors: mrlthf11
+ * @Date: 2021-03-21 12:40:32
+ * @LastEditTime: 2021-03-21 13:27:13
+ * @Description: file content
+ */
+
+const a = 5;
+const b = 5;
+
+console.log(a);
+console.log(b);
